f235155ea2f573803be3b3ea9d9229e374a44181
Alexander <84857900+4z0t@users.noreply.github.com>
2023-06-09 14:59:25 +0300

    update 8.c

--- 8.c
+++ 8.c
@@ -1,5 +1,21 @@
+#include <stdint.h>
+#define SW 0x01000000
+#define LED 0x02000000
 
 int main()
 {
+    uint32_t *sw_addr = (uint32_t *)SW;
+    uint32_t *led_addr = (uint32_t *)LED;
+    const uint32_t v = 5;
+    while (1)
+    {
+        while (*sw_addr)
+        {
+            if (*sw_addr > v)
+                *led_addr = *sw_addr + (v >> 1);
+            else
+                *led_addr = ((*sw_addr) >> 1) + v;
+        }
+    }
     return 0;
 }
--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 
 set gcc_=C:\Users\gaste\Downloads\xpack-riscv-none-elf-gcc-12.2.0-3-win32-x64\xpack-riscv-none-elf-gcc-12.2.0-3\bin\riscv-none-elf-gcc
 set objcopy_=C:\Users\gaste\Downloads\xpack-riscv-none-elf-gcc-12.2.0-3-win32-x64\xpack-riscv-none-elf-gcc-12.2.0-3\bin\riscv-none-elf-objcopy
-
+set objdump_=C:\Users\gaste\Downloads\xpack-riscv-none-elf-gcc-12.2.0-3-win32-x64\xpack-riscv-none-elf-gcc-12.2.0-3\bin\riscv-none-elf-objdump
 %gcc_% -c -march=rv32i -mabi=ilp32 startup.S -o out/startup.o
 %gcc_% -c -march=rv32i -mabi=ilp32 8.c -o out/main.o
 %gcc_% -march=rv32i -mabi=ilp32 -Wl,--gc-sections -nostartfiles -T linker_script.ld out/startup.o out/main.o -o out/result.elf
 %objcopy_% -O verilog out/result.elf out/init.mem
 %objcopy_% -O verilog -j .text out/result.elf out/init_instr.mem
 %objcopy_% -O verilog -j .data -j .bss out/result.elf out/init_data.mem
+%objdump_% -D out/result.elf > out/dis.S
